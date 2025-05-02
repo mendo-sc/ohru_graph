6acd48013b23ad046ca63ae03b4aa92028a3ce6b
mendo-sc <mendo@student.cscc.edu>
2025-05-02 00:06:23 -0400

    Enumerate function to show header information

--- unemp_rate.py
+++ unemp_rate.py
@@ -5,3 +5,10 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 path = Path('data/OHUR.csv')
+lines = path.read_text(encoding='utf-8').splitlines()
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# Header information
+for index, column_head in enumerate(header_row):
+    print(index, column_head)
